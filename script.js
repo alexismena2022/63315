530c468e3a5edb40c62122a251c33cb13dcd288b
Alexis Mena <alexis.mena.borealit@gmail.com>
2024-10-12 17:20:02 -0300

    se modifica linea 6

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let apellido = "perez"
 let edad = 30
 let sobrenombre = " usted es muy bueno"
 
-let nombreadjunto = prompt ("cual es su nombre?")
+let nombreadjunto = prompt ("cual es tu nombre?")
 alert ("bienvendo " + nombreadjunto + sobrenombre)
