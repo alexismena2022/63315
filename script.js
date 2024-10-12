73c30395fa3fae806f31ff8d4a87383228ae25bb
Alexis Mena <alexis.mena.borealit@gmail.com>
2024-10-12 16:03:27 -0300

    Se crean Los archivos Para la comision 63315
    Estos archivos son index.html y script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombre = "juan"
+let apellido = "perez"
+let edad = 30
+let sobrenombre = " usted es puto"
+
+let nombreadjunto = prompt ("cual es su nombre?")
+alert ("bienvendo " + nombreadjunto + sobrenombre)
