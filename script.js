42d466e8b1f540cb60ba891f5ec432e82777579b
Alexis Mena <alexis.mena.borealit@gmail.com>
2024-10-12 16:37:53 -0300

    se modifica frase de pruebas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let nombre = "juan"
 let apellido = "perez"
 let edad = 30
-let sobrenombre = " usted es puto"
+let sobrenombre = " usted es muy bueno"
 
 let nombreadjunto = prompt ("cual es su nombre?")
 alert ("bienvendo " + nombreadjunto + sobrenombre)
