0a74f7370df47c4f39eea41f054355bbc5d6778b
Alexis Mena <alexis.mena.borealit@gmail.com>
2024-10-14 20:12:58 -0300

    se agrega very in line 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let nombre = "juan"
 let apellido = "pepe"
 let edad = 30
-let sobrenombre = " usted es muy bueno"
+let sobrenombre = " usted es very bueno"
 
 let nombreadjunto = prompt ("cual es tu nombre?")
 alert ("bienvendo " + nombreadjunto + sobrenombre)
